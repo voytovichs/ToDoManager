16dab00918d0f1336ba4759860d05a6b0d498d9a
voytovich sergey <voytovich.sergeey@gmail.com>
2015-07-05 10:55:13 +0300

    First step in making Activity that will create an task item and also layout file for this purpose

---
+++ app/src/main/java/com/voytovichs/todomanager/AddTaskItemActivity.java
@@ -0,0 +1,17 @@
+package com.voytovichs.todomanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by voytovichs on 05.07.15.
+ */
+public class AddTaskItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task_layout);
+    }
+
+}
