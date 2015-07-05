b47ed8fce868439aa3008787c2c49cdf8f5c3392
voytovich sergey <voytovich.sergeey@gmail.com>
2015-07-05 20:24:42 +0300

    Correct keyboard showing/hiding Deleting TextView with title above task name EditText

--- app/src/main/java/com/voytovichs/todomanager/AddTaskItemActivity.java
+++ app/src/main/java/com/voytovichs/todomanager/AddTaskItemActivity.java
@@ -1,17 +1,46 @@
 package com.voytovichs.todomanager;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by voytovichs on 05.07.15.
  */
 public class AddTaskItemActivity extends Activity {
 
+    private EditText mTittle = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_task_layout);
+
+        mTittle = (EditText) findViewById(R.id.titleEditText);
+
+        mTittle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showKeyboard();
+            }
+        });
+
+
+    }
+
+    private void showKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+    }
+
+    private void hideKeyboardFromTitle(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mTittle.getWindowToken(),0);
     }
 
 }
