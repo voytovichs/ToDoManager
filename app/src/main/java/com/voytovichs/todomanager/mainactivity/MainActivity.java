07dd2386b2c55d30edca6e388067c0ca973e67e0
voytovich sergey <voytovich.sergeey@gmail.com>
2015-07-12 00:20:54 +0300

    Nice add task button icon

--- app/src/main/java/com/voytovichs/todomanager/mainactivity/MainActivity.java
+++ app/src/main/java/com/voytovichs/todomanager/mainactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.voytovichs.todomanager.mainactivity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 
@@ -10,6 +13,8 @@ import com.voytovichs.todomanager.mainactivity.layouts.FloatingActionButton;
 
 
 public class MainActivity extends Activity {
+
+    private static int BUTTON_ICON_SIZE = 70;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +26,12 @@ public class MainActivity extends Activity {
     }
 
     private void setFloatingButton() {
+
+        Drawable dr = getResources().getDrawable(R.drawable.add_button);
+        Bitmap bitmap = ((BitmapDrawable) dr).getBitmap();
+        Drawable buttonIcon = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, BUTTON_ICON_SIZE, BUTTON_ICON_SIZE, true));
         FloatingActionButton fabButton = new FloatingActionButton.Builder(this)
-                .withDrawable(getResources().getDrawable(R.drawable.add_button))
-                        //.withButtonColor(R.color.primaryColor)
+                .withDrawable(buttonIcon)
                 .withButtonColor(getResources().getColor(R.color.primaryColor))
                 .withGravity(Gravity.BOTTOM | Gravity.RIGHT)
                 .withMargins(0, 0, 16, 16)
